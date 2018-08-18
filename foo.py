1675e8c3106312fd92921b02842f6209f363abfc
izzybitsie <42483624+izzybitsie@users.noreply.github.com>
2018-08-17 18:44:24 -0700

    Refactor mul2. (#1)

--- foo.py
+++ foo.py
@@ -3,6 +3,10 @@
 def mul2(num):
     return 2*num
 
+def print_times(num):
+    for n in range(num):
+        print(f"{n} multiplied by 2 is {mul2(n)}")
+
+
 if __name__ == "__main__":
-    for num in range(10):
-        print(f"{num} multiplied by 2 is {mul2(num)}")
+    print_times(10)
